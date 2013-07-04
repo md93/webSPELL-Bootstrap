fe1841e9cc02c4e2dbba9846cc2ff9f8d6485d75
Joshua <github@sky-e.de>
2013-07-04 15:48:23 +0200

    fixing tablet view and changing grid width to 9
    news needs row-fluid to adapt to new grid size

--- js/wSBs.js
+++ js/wSBs.js
@@ -18,5 +18,16 @@
       selector: "a[data-toggle=tooltip]",
       html: true,
       container: 'body'
-    })
+  })
+
+  var leftcol = $('div#leftcol');
+  var maincol = $('div#maincol');
+
+  if(leftcol.is(':hidden')) {
+    maincol.removeClass('push3 span6').addClass('span9');
+  }
+  else if(leftcol.is(':visible') && !maincol.hasClass('push3') || maincol.hasClass('span9')) {
+    maincol.addClass('push3 span6').removeClass('span9');
+  }
+
 })(jQuery);
